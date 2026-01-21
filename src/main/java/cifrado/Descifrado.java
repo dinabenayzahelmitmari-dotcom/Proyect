559c6c2595b951270ce8ec353f4475ee0d80f4ca
Ricardo <richi2548@gmail.com>
2026-01-21 23:37:50 +0100

    Servidor y Descifrado

--- src/main/java/cifrado/Descifrado.java
+++ src/main/java/cifrado/Descifrado.java
@@ -1,4 +1,20 @@
 package cifrado;
 
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
 public class Descifrado {
+
+    private static final String ALGORITMO = "AES";
+    private static final String CLAVE = "1234567890123456"; // Igual que en el cliente
+
+    public static String descifrar(String textoCifrado) throws Exception {
+        SecretKeySpec key = new SecretKeySpec(CLAVE.getBytes(), ALGORITMO);
+        Cipher cipher = Cipher.getInstance(ALGORITMO);
+        cipher.init(Cipher.DECRYPT_MODE, key);
+        byte[] bytesDecodificados = Base64.getDecoder().decode(textoCifrado);
+        byte[] textoOriginal = cipher.doFinal(bytesDecodificados);
+        return new String(textoOriginal);
+    }
 }
--- src/main/java/servidor/Servidor.java
+++ src/main/java/servidor/Servidor.java
@@ -1,4 +1,33 @@
 package servidor;
 
+import cifrado.Descifrado;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Servidor {
+
+    public static void main(String[] args) {
+        int puerto = 5000;
+
+        try (ServerSocket serverSocket = new ServerSocket(puerto)) {
+            System.out.println("Servidor escuchando en el puerto " + puerto);
+
+            Socket socket = serverSocket.accept();
+            System.out.println("Cliente conectado");
+
+            BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String mensajeCifrado = input.readLine();
+            System.out.println("Mensaje recibido (cifrado): " + mensajeCifrado);
+
+            // Descifrar mensaje
+            String mensajeOriginal = Descifrado.descifrar(mensajeCifrado);
+            System.out.println("Mensaje descifrado: " + mensajeOriginal);
+
+            socket.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
